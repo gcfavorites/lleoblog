ab1b9ae1dd58afed6e5565b1435cd5b581ae37fd
lleo <lleo@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2009-12-09 09:54:49 +0000

    Аццкие глюки имблоада побеждены!

--- ajax_imbload.php
+++ ajax_imbload.php
@@ -1,5 +1,4 @@
-<?
-
+<? // протокол IMBLOAD - отдача заметки поаяксу любому пользователю
 include "config.php";
 include $include_sys."_autorize.php";
 include $include_sys."_msq.php";
@@ -7,15 +6,31 @@ include $include_sys."_onetext.php";
 include $include_sys."_modules.php";
 require_once $include_sys."JsHttpRequest.php"; $JsHttpRequest =& new JsHttpRequest("windows-1251");
 
-$p=ms("SELECT `Access`,`Header`,`num`,`Body` FROM `dnevnik_zapisi` WHERE `Date`='".e($_REQUEST["Date"])."'","_1");
+$p=ms("SELECT `Access`,`Header`,`num`,`Body` FROM `dnevnik_zapisi` ".WHERE("`Date`='".e($_REQUEST["Date"])."'"),"_1");
 
 if($p===false) otprav("Error 404");
 
 $_RESULT["Admin"] = $admin_name;
 $_RESULT["Header"] = zamok($p["Access"]).$p["Header"];
-$_RESULT["Body"] = obr_link(onetext($p));
+$_RESULT["Body"] = obr_link(blogtags(onetext($p)))
+
+ .htmlspecialchars($_RESULT["Body"])
+
+;
 $_RESULT["status"] = true;
 exit;
+
+
+function blogtags($s) { global $httpsite;
+//return $s;
+return str_replace(
+	array('{foto_www_preview}','{foto_www_small}'),
+	array($httpsite.$GLOBALS['foto_www_preview'],$httpsite.$GLOBALS['foto_www_small']),
+//	array('http://#############','http://############'),
+
+$s);
+
+}
 
 
 function otprav($s) { global $_RESULT,$msqe; $_RESULT["otvet"] = $msqe.$s; $_RESULT["status"] = true; exit; }
@@ -24,9 +39,10 @@ function otprav($s) { global $_RESULT,$msqe; $_RESULT["otvet"] = $msqe.$s; $_RES
 function obr_link($s) { 
 	$s=preg_replace_callback("/(<img[^>]+src=[\'\"]*)((?!http)[^\'\"\s]+)/si","obr_link1",$s);
 	$s=preg_replace_callback("/(<a[^>]+href=[\'\"]*)((?!http)[^\'\"\s]+)/si","obr_link1",$s);
+	$s=preg_replace_callback("/(onclick=\'return\sfoto\(\")((?!http)[^\s\"]+)/si","obr_link1",$s);
 	return $s;
 }
-function obr_link1($s) { return $s[1].$GLOBALS['httphost'].$s[2]; }
+function obr_link1($s) { return $s[1].$GLOBALS['httpsite'].$s[2]; }
 
 
 function zamok($d) {
@@ -35,5 +51,9 @@ function zamok($d) {
         if($d=='podzamok') return $z;
         return $z.$z;
 }
+
+
+function WHERE($s='') { global $access; if($s.$access=='') return ''; if($s=='' || $access=='') return "WHERE ".$s.$access; return "WHERE ".$s." AND ".$access; }
+
 
 ?>
