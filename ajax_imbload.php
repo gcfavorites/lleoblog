a799a6299d4bf205db2e3520fbffe54031fb1fca
lleo <lleo@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2009-12-20 12:01:39 +0000

    Peredelal vse nah!

--- ajax_imbload.php
+++ ajax_imbload.php
@@ -5,17 +5,25 @@ include $include_sys."_msq.php";
 include $include_sys."_onetext.php";
 include $include_sys."_modules.php";
 
-
 if($_GET['mode']=='oembed') {
 	$p=get_zametka($_GET['date']); if($p===false) idie("Error 404");
 	send_oembed($p);
-} else {
-	require_once $include_sys."JsHttpRequest.php"; $JsHttpRequest =& new JsHttpRequest($wwwcharset);
-	$p=get_zametka($_REQUEST["Date"]); if($p===false) otprav_ajax("Error 404");
-	send_ajax($p);
 }
 
+
+if($_GET['mode']=='xml') {
+	$p=get_zametka($_GET['date']); if($p===false) idie("Error 404");
+	send_xml($p);
+}
+
+
+// а иначе ajax
+require_once $include_sys."JsHttpRequest.php"; $JsHttpRequest =& new JsHttpRequest($wwwcharset);
+$p=get_zametka($_REQUEST["Date"]); if($p===false) otprav_ajax("Error 404");
+send_ajax($p);
+
 exit;
+
 
 function get_zametka($Date) {
 	$p=ms("SELECT `DateDate`,`Access`,`Header`,`num`,`Body` FROM `dnevnik_zapisi` ".WHERE("`Date`='".e($Date)."'"),"_1");
@@ -24,6 +32,7 @@ function get_zametka($Date) {
 	$p["Header"] = zamok($p["Access"]).$Date.($p["Header"]?" - ".$p["Header"]:'');
 	$p["Body"] = obr_link(blogtags(onetext($p)));
 	$p["Date"] = $Date;
+	$p["url"] = $GLOBALS["httphost"].$p['Date'].($p['DateDate']?".html":'');
 	return $p;
 }
 
@@ -38,16 +47,33 @@ function send_ajax($p) { $GLOBALS['_RESULT']=array(
 }
 
 function send_oembed($p) {
+	header("Content-Type: text/plain; charset='".$GLOBALS['wwwcharset']."'");
 	die('{"provider_name": "lleoblog",'
 	.'"provider_url": "'.s($GLOBALS["httphost"]).'",'
 	.'"author_name": "'.s($p["Admin"]).'",'
-	.'"author_url": "'.s($GLOBALS["httphost"]).$p['Date'].($p['DateDate']?".html":'').'",'
+	.'"author_url": "'.s($p['url']).'",'
 	.'"title": "'.s($p["Header"]).'",'
 	.'"html": "'.s($p["Body"]).'",'
 	.'"height": 1024,'
 	.'"width": 768,'
 	.'"version": "1.0",'
 	.'"type": "link"}');
+}
+
+
+
+function send_xml($p) { global $wwwcharset;
+	header("Content-Type: text/xml; charset='".$wwwcharset."'");
+	die('<?xml version="1.0" encoding="'.$wwwcharset.'" standalone="yes"?>'
+	.'<oembed><version>1.0</version>'
+	.'<provider_name>lleoblog</provider_name>'
+	.'<provider_url>'.htmlspecialchars($GLOBALS["httphost"]).'</provider_url>'
+	.'<author_name>'.htmlspecialchars($p["Admin"]).'</author_name>'
+	.'<author_url>'.htmlspecialchars($p["url"]).'</author_url>'
+	.'<title>'.htmlspecialchars($p["Header"]).'</title>'
+	.'<type>link</type>'
+	.'<html>'.htmlspecialchars($p["Body"]).'</html>'
+	.'</oembed>');
 }
 
 function s($s) { return addcslashes(
