064b63f505d5d454f57035edcbe30007a3c8d4d9
kns.tmb@gmail.com <kns.tmb@gmail.com@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2010-02-07 20:32:28 +0000

    modified   ajax/comment.php modified   ajax/editor.php modified   css/blog.css modified   module/comm.php modified   site_module/OTHERBLOGS.php modified   site_module/PREWORD.php modified   update_veto_files.txt

--- site_module/OTHERBLOGS.php
+++ site_module/OTHERBLOGS.php
@@ -2,14 +2,14 @@
 
 function OTHERBLOGS($e) {
 
-if($GLOBALS['httpsite'] == "http://lleo.aha.ru") { // только для сайта блогодвижка, остальным этот модуль удалить
-$l=file_get_contents($GLOBALS['host_log']."blogs.txt"); $m=explode("\n",$l); $s=$a='';
-foreach($m as $l) { $l=h($l); if($l!='') { list($link,$admname)=explode(' ',$l,2);
-	$a.= "<br><a href='$link'>".($admname!=''?$admname:$link)."</a>";
-}} if($a!='') $s .= "<div style='text-align: left; font-size:11px;'><p>другие блоги:<br>$a</div>";
-}
 
-return $s;
+
+
+
+
+
+
+return '';
 
 }
 
