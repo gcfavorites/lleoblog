a0b437a9e22b6d5fb991f13a392a08f766671401
kns.tmb@gmail.com <kns.tmb@gmail.com@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2010-02-19 07:12:43 +0000

    modified   site_module/REKOMENDA.php

--- site_module/REKOMENDA.php
+++ site_module/REKOMENDA.php
@@ -3,8 +3,10 @@
 function REKOMENDA($e) { $s=''; // return $s;
 	$pp=ms("SELECT `link`,`text`,`datetime` FROM ".$GLOBALS['db_rekomenda']." WHERE `datetime`>(NOW()- INTERVAL 2 DAY) ORDER BY `datetime` DESC","_a");
 	if(sizeof($pp)) {
-		foreach($pp as $p) $s.="<div>".$p['datetime'].": <a href=".$p['link'].">".$p['text']."</a></div>";
-		return "<div class='rekomenda'>".$s."</div>";
+		foreach($pp as $p) {
+			list($date,)=explode(' ',$p['datetime']);
+			$s.="<div>".$date." <a href=".$p['link'].">".$p['text']."</a></div>";
+		} return "<div class='rekomenda'>".$s."</div>";
 	}
 	return $s;
 }
