cb279e17f2940e7236c62161091b6c1a25b15bf2
kns.tmb@gmail.com <kns.tmb@gmail.com@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2010-03-12 03:50:21 +0000

    modified   site_module/MAIN.php modified   template/blog.html modified   template/panel_comment.php

--- template/panel_comment.php
+++ template/panel_comment.php
@@ -6,10 +6,10 @@ $panel = strtr("
 <img class=knop onClick=\"pns('".$id."','".chr(169)."','')\" src=".$www_design."e2/copy.gif>
 <img class=knop onClick=\"pns('".$id."','".chr(151)."','')\" src=".$www_design."e2/mdash.gif>
 <img class=knop onClick=\"pns('".$id."','".chr(171)."','".chr(187)."')\" src=".$www_design."e2/ltgt.gif>
-<img class=knop onClick=\"pns('".$id."','<b>','</b>')\" src=".$www_design."e2/bold.gif>
-<img class=knop onClick=\"pns('".$id."','<i>','</i>')\" src=".$www_design."e2/italic.gif>
-<img class=knop onClick=\"pns('".$id."','<s>','</s>')\" src=".$www_design."e2/strikethrough.gif>
-<img class=knop onClick=\"pns('".$id."','<u>','</u>')\" src=".$www_design."e2/underline.gif>
+<img class=knop onClick=\"pns('".$id."','[b]','[/b]')\" src=".$www_design."e2/bold.gif>
+<img class=knop onClick=\"pns('".$id."','[i]','[/i]')\" src=".$www_design."e2/italic.gif>
+<img class=knop onClick=\"pns('".$id."','[s]','[/s]')\" src=".$www_design."e2/strikethrough.gif>
+<img class=knop onClick=\"pns('".$id."','[u]','[/u]')\" src=".$www_design."e2/underline.gif>
 </div>
 ","\n","");
 
