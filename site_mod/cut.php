120d44d515c628ba58501b531b97d7a0f45c2772
kns.tmb <kns.tmb@gmail.com>
2010-04-23 18:09:22 +0000

    modified   include_sys/_autorize.php modified   site_mod/cut.php added      site_mod/SILK_TEST.php

--- site_mod/cut.php
+++ site_mod/cut.php
@@ -16,11 +16,36 @@
 
 // if(!isset($GLOBALS['admin_name'])) die(highlight_file(__FILE__,1));
 
+function cut($e) {
+
+/*
+$conf=array_merge(array(
+'nskip'=>5,
+'next'=>"<small><a href={nextpage}>&lt;&lt;&nbsp;предыдущие {n}</a></small>",
+'prev'=>"<small><a href={prevpage}>следующие {n}</a>&nbsp;&gt;&gt;</small>",
+'prevnext'=>"<table width=100%><tr><td align=left>{next}</td><td align=right>{prev}</td></tr></table><p>",
+// 'comment'=>"<p align=right><a style='font-size:10pt;' href={link}#comments>Добавить комментарий</a> <small>(сейчас {ncomm} шт)</small></p
+'comment'=>"comment = <div style='text-align: right; font-size:10pt; margin-right: 5px'><a href={link}#comments>комментариев {ncomm}</a> | <
+'template'=>"<div style='text-align:justify;padding:0 15px;'><div class='header' id='Header_{num}' style='text-align:left'>{edit}<a href='{l
+),parse_e_conf($e));
+
+$LAST_skip = intval($conf['nskip']); // 5;
+
+$skip=intval($_GET['skip']);
+$pp=ms("SELECT `Date`,`Body`,`template`,`autokaw`,`autoformat`,`Header`,`DateUpdate`,`Access`,`num`,`Comment_view` FROM `dnevnik_zapisi` ".W
+
+$n=sizeof($pp);
+if($n>$LAST_skip){ unset($pp[$n-1]); $next=mper($conf['next'],array('nextpage'=>$mypage."?skip=".($skip+$LAST_skip).$catpn,'n'=>$LAST_skip))
+$n=$skip-$LAST_skip;
+if($n>=0) { $prev=mper($conf['prev'],array('prevpage'=>$mypage."?skip=".$n.$catpn,'n'=>$LAST_skip)); } else $prev='';
+*/
+
+
+
 if($GLOBALS['article']['autoformat']=='no') { $GLOBALS['cutar1']=array("&","\\","'",'"',"\n","\r"); $GLOBALS['cutar2']=array("&amp;","\\\\","\\'",'&quot;',"\\n",""); }
 elseif($GLOBALS['article']['autoformat']=='p') { $GLOBALS['cutar1']=array("&","\\","'",'"',"\n ","\n\n","\n","\r"); $GLOBALS['cutar2']=array("&amp;","\\\\","\\'",'&quot;',"\n<p class=z>","<p>","<br>",""); }
 else { $GLOBALS['cutar1']=array("&","\\","'",'"',"\n ","\n\n","\n","\r"); $GLOBALS['cutar2']=array("&amp;","\\\\","\\'",'&quot;',"\n<p class=z>","<p class=pd>","<p class=d>",""); }
 
-function cut($e) {
 
 	$tag="[показать&nbsp;спрятанное]";
 	$tag0="[...]";
