b5fbc6e55ce1f770655ce6941b54efb891c0623c
kns.tmb@gmail.com <kns.tmb@gmail.com@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2011-07-16 17:52:35 +0000

    update

--- install.php
+++ install.php
@@ -10,11 +10,15 @@ ini_set("display_startup_errors","1");
 ini_set('error_reporting', E_ALL);
 set_time_limit(0);
 
-// проверка, есть ли библиотека curl (извините, у меня работает через нее, заебался с этими fsockopen) и другие
+
 if(!function_exists('file_put_contents')) { function file_put_contents($url,$s) { $f=fopen($url,"w"); fputs($f,$s); fclose($f); } }
 
 //	die("<p>Fatal error: '$s' not found! Install in Apache/PHP!!!");
-$s='curl_init'; if(function_exists($s)) idebug("CURL module - ok"); else die("<p>Fatal error: CURL not found! Install CURL module in Apache/PHP.");
+
+// проверка, есть ли библиотека curl (извините, у меня работает через нее, заебался с этими fsockopen) и другие
+// UPD: не, не заебался, разобрался. НАХУЙ CURL!!!!!!!!!!!
+// $s='curl_init'; if(function_exists($s)) idebug("CURL module - ok"); else die("<p>Fatal error: CURL not found! Install CURL module in Apache/PHP.");
+
 $s='iconv'; if(function_exists($s)) idebug("ICONV module - ok"); else die("<p>Fatal error: ICONV not found! Install ICONV-module in Apache/PHP.");
 $s='ImageCreateFromJpeg'; if(function_exists($s)) idebug("GB module - ok"); else idebug("Warning: GD not found, install GD-module in Apache/PHP (for working with photoalbum).");
 
@@ -421,19 +425,117 @@ function get_veto_files($f,$x=1) { global $filehost; $a=array(); if(is_file($f))
 function strstr_true($s,$c) { $a=explode($c,$s); return ($a[sizeof($a)-1]); }
 
 //---------------------------
-function curl_post($url, $post) { 
-	$p=array(); foreach($post as $n=>$v) $p[]=$n.'='.urlencode($v);
-	$ch=curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-//	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, join('&', $p));
-	$c=curl_exec($ch);
-	if(curl_errno($ch)!=0) $c=false;
-	curl_close($ch);
-	return $c; 
+function curl_post($url, $post) {
+
+	// Заменим на процедуру без curl:
+
+        //==================================================================================================
+        // процедура передачи данных и файлов через POST-запрос по старинке без всяких там уебищных CURL-библиотек
+        // $filePath - полное имя (с путем) файла для передачи или массив имен файлов для передачи (если файлов нет - '')
+        // $urla - адрес запроса, напр. http://lleo.aha.ru/blog/install
+        // $ara - массив переменных POST, напр: array('action'=>'do','key'=>'1','user'=>123)
+        // возвращает ответ сервера или, если ошибка, строку, начинающуюся с 'ERROR:'
+        $o=POST_file('',$url,$post);
+        if(substr($o,0,6)=='ERROR:') { echo "<hr>$o "; return false; }
+        return $o;
+
+/*
+	А вот это curl-говно было раньше:
+
+        $p=array(); foreach($post as $n=>$v) $p[]=$n.'='.urlencode($v);
+        $ch=curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+//      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, join('&', $p));
+        $c=curl_exec($ch);
+        if(curl_errno($ch)!=0) $c=false;
+        curl_close($ch);
+        return $c;
+*/
 }
+
+//==================================================================================================
+// процедура передачи данных и файлов через POST-запрос по старинке без всяких там уебищных CURL-библиотек
+// $filePath - полное имя (с путем) файла для передачи или массив имен файлов для передачи (если файлов нет - '')
+// $urla - адрес запроса, напр. http://lleo.aha.ru/blog/install
+// $ara - массив переменных POST, напр: array('action'=>'do','key'=>'1','user'=>123)
+// возвращает ответ сервера или, если ошибка, строку, начинающуюся с 'ERROR:'
+function POST_file($filePath,$urla,$ara,$port=80,$scheme='http',$charset='Windows-1251') {
+        if(gettype($filePath)!='array') $filePath=array($filePath);
+        $url=array_merge(array('scheme'=>$scheme,'port'=>$port),parse_url($urla));
+        $bu="---------------------".substr(md5($filePath.rand(0,32000)),0,10); $r="\r\n"; $ft=$r.'--'.$bu.'--'.$r;
+
+        // данные
+        $dat=''; if(count($ara)) foreach($ara as $n=>$v) $dat.='--'.$bu.$r.'Content-Disposition: form-data; name="'.$n
+.'"'.$r.$r.urlencode($v).$r;
+
+        $len=strlen($dat); // общая длина
+
+        $files=array(); $k=0; foreach($filePath as $l) { if(empty($l)) continue;
+                if(!is_file($l)) return "ERROR: file not found '$l'";
+                $fh='--'.$bu.$r
+                .'Content-Disposition: form-data; name="file'.(++$k).'"; filename="'.urlencode(basename($l)).'"'.$r
+                .'Content-Type: '.$charset.$r
+                .$r;
+
+                $len+=strlen($fh.$ft)+filesize($l);
+                $files[$l]=$fh;
+        }
+
+        $headers="POST ".$urla." HTTP/1.0".$r
+        ."Host: ".$url['host'].$r
+        ."Referer: ".$url['host'].$r
+        ."Content-type: multipart/form-data, boundary=".$bu.$r
+        ."Content-length: ".$len.$r
+        .$r
+        .$dat;
+
+        // открыть хост
+        if(!$fp=fsockopen($url['host'],$url['port'])) return "ERROR: can't open url ".$url['host'].":".$url['port'];
+        // запихнуть заголовок и POST-массив
+        if(fputs($fp,$headers)===false) return "ERROR: can't send #1";
+
+        if(count($files)) foreach($files as $l=>$fh) { // позапихивать файлы
+                if(fputs($fp,$fh)===false) return "ERROR: can't send #2";
+                // открыть файл и запихнуть его
+                if(($fp2=fopen($l,"rb"))===false) return "ERROR: can't open file '".$l."'";
+                while(!feof($fp2)) if(fputs($fp,fgets($fp2,1024*100))===false) return "ERROR: can't send #4";
+                fclose($fp2);
+                // запихнуть заключительный хедер
+                if(fputs($fp,$ft)===false) return "ERROR: can't send #5";
+        }
+
+        // и получить ответ
+        $s=''; while(!feof($fp)) $s.=fgets($fp,4096); fclose($fp);
+        if($s=='') return "ERROR: NO RESPONSE";
+        list($h,$t)=explode($r.$r,$s,2);
+
+        // обработка переноса
+        if(stristr($h,'301 Moved Permanently')) {
+                return POST_file($filePath,preg_replace("/^.+Location: ([^\s]+).*$/si","$1",$h),$ara);
+        }
+
+return $t;
+}
+//==================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function idebug($s) { if(sizeof($_GET) or sizeof($_POST)) return; else print "<br><font color=gray size=2><i>$s</i></font>"; }
