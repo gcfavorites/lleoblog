4ef4466f7bc6f28e9d14fce2aeea19f21ada3b43
kns.tmb <kns.tmb@gmail.com>
2011-08-15 19:52:44 +0000

    update

--- include_sys/install_update.php
+++ include_sys/install_update.php
@@ -100,39 +100,67 @@ $GLOBALS['selectjs']="
 
 i_seldir=function(e){ alert(e.tagName); };
 
-go_install=function(){ var tr=idd('i_selectfiles').getElementsByTagName('TR');
+i_sett=function(e,t){
+	e.style.cursor='pointer';
+	e.style.textDecoration='none';
+
+	e.setAttribute('tiptitle',t);
+	addEvent(e,'mouseover',function(){ idd('tip').innerHTML=this.getAttribute('tiptitle'); posdiv('tip',mouse_x+10,mouse_y+10); });
+	addEvent(e,'mouseout',function(){ zakryl('tip') } );
+	addEvent(e,'mousemove',function(){ posdiv('tip',mouse_x+10,mouse_y+10) } );
+}
+
+go_install=function(){ var t,tr=idd('i_selectfiles').getElementsByTagName('TR');
 
 	for(var i=0;i<tr.length;i++){ var p=tr[i];
 		var td1=p.firstChild; var td2=p.lastChild; if(td2==td1) continue;
 
+			var dir=td1.firstChild;
+				dir.onclick=function(){i_change_dir(this)};
+				i_sett(dir,'Select/Unselest all files');
+
 			var ee=td2.getElementsByTagName('DIV');
 			for(var j=0;j<ee.length;j++){ var pp=ee[j];
-				var l=pp.innerHTML; var O=l.substring(0,1);
-				pp.innerHTML=l.substring(1,l.length);
+				var l=pp.innerHTML; var O=l.substring(0,1); l=l.substring(1,l.length);
+
+				pp.setAttribute('myl',l);
+				pp.setAttribute('myO',O);
+
+				pp.innerHTML='<br>'+l;
 				pp.onclick=function(){i_change(this)};
 				pp.style.display='inline';
-				pp.style.styleFloat='left';
-				if(O=='U') { pp.style.color='green'; var t='update'; }
-				else if(O=='A') { pp.style.color='red'; var t='del'; }
-				else { pp.style.color='magenta'; var t='unk'; }
 
-					pp.setAttribute('tiptitle',t);
-					addEvent(pp,'mouseover',function(){ idd('tip').innerHTML=this.getAttribute('tiptitle'); posdiv('tip',mouse_x+tip_x,mouse_y+tip_y); });
-					addEvent(pp,'mouseout',function(){ zakryl('tip') } );
-					addEvent(pp,'mousemove',function(){ posdiv('tip',mouse_x+10,mouse_y+10) } );
+				if(O=='U') { pp.style.color='green'; t='update'; }
+				else if(O=='A') { pp.style.color='red'; t='del'; }
+				else { pp.style.color='magenta'; t='unk'; }
+
+				i_sett(pp,t);
+
 				}
-
-			/* td1.style.fontWeight='bold'; td1.style.fontSize='28px;'; */
 	}
 	return;
 };
 
 setTimeout(go_install,500);
 
-i_getopt=function(e){ var c=e.style.color; return e.getAttribute('tiptitle'); };
+i_getopt=function(e){ var c=e.style.color; return e.getAttribute('tiptitle'); 
+alert(e.innerHTML+' '+i_getopt(e)+' '+e.style.textDecorationLineThrough);
+};
 
-i_change=function(e){ alert(e.innerHTML+' '+i_getopt(e)); };
 
+i_change_dir=function(e){ var p=e.parentNode.nextSibling.getElementsByTagName('DIV');
+	for(var i=0;i<p.length;i++){i_change(p[i])}
+};
+
+i_change=function(e){ 
+	if(e.style.textDecoration=='none') {
+		e.style.textDecoration='line-through';
+		e.innerHTML=e.innerHTML.replace(/^<br>(.+?)$/g,'<br>&nbsp;&nbsp;$1&nbsp;&nbsp;');
+	} else {
+		e.style.textDecoration='none';
+		e.innerHTML=e.innerHTML.replace(/\&nbsp;/g,' ').replace(/^<br> +(.+?) +$/g,'<br>$1');
+	}
+};
 
 
 i_all=function(){
@@ -158,7 +186,7 @@ i_f=function(e){ var c=e.className.split(' ');
 	e.className=c.join(' ');
 };
 ";
-	return "<input type='button' onclick='go_install()' value='INSTALL'><div id='i_selectfiles'>$s</div>";
+	return "<P onclick='this.style.textDecorationLineThrough=true;'>Click this if you</p><input type='button' onclick='go_install()' value='INSTALL'><div id='i_selectfiles'>$s</div>";
 }
 
 ?>
