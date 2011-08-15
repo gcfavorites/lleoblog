fa53c42815afe6d9dc01dc2423cb8e3b9d1c771f
kns.tmb@gmail.com <kns.tmb@gmail.com@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2011-08-15 22:52:44 +0000

    update

--- include_sys/install_update.php
+++ include_sys/install_update.php
@@ -27,7 +27,7 @@ function UPDATE_select($rrr) { $r=unserialize($rrr); // return "<pre>".print_r($
 		$Ufile[$file]=$val;
 	}
 
-/*
+//=========================================================
 	// 1. Что с конфигом?
 	// config:msq_login $msq_login = ""; // "lleo";
 	$con=file_get_contents('config.php'); preg_match_all("/\n\s*".'\$'."([0-9a-z\_\-]+)\s*\=\s*([^\n]+)/si",$con,$m);
@@ -36,45 +36,26 @@ function UPDATE_select($rrr) { $r=unserialize($rrr); // return "<pre>".print_r($
 	foreach($Uconf as $n=>$v) { if(isset($con[$n])) unset($con[$n]); else $s.="<div>".'A'.'$'.$n."</div>"; }
 	foreach($con as $n=>$l) { $s.="<div>".'D'.'$'.$n."=".h($l)."</div>"; } // предлагается удалить
 	unset($con);
-*/
+//=========================================================
 	// 2. Что с языком?
 	// lang:fido/ru:Comments:empty_comm Comments:empty_comm	А где же комментарий?
-	$lan=array();
-	foreach($Ulang as $n=>$v) { list($ll,$per)=explode(':',$n,2);
+	$lan=array(); $allan=array();
+	foreach($Ulang as $n=>$v) { list($ll,$per)=explode(':',$n,2); $allan[$ll]='';
 
-		if(!isset($lanz[$ll])) { // закачать сразу язык шоб не париться
+		if(!isset($lan[$ll])) { $lan[$ll]=array(); // закачать сразу язык шоб не париться
 		$nf=$GLOBALS['filehost'].'binoniq/lang/'.$ll.".lang";
-		if(is_file($nf)&&($li=file($nf))!='') { $lan[$ll]=array();
-		  foreach($li as $c) { list($cn,$cv)=explode("\t",$c,2);
-			if(($cn=trim($cn))=='') continue; $lan[$ll][$cn]=trim($cv);
-			}
+		if(is_file($nf)&&($li=file($nf))!='') foreach($li as $c) {
+			list($cn,$cv)=explode("\t",$c,2); if(($cn=trim($cn))=='') continue; $lan[$ll][$cn]=trim($cv);
 		}}
 
-		return "<pre>#########".print_r($lan[$ll],1);
-
+		if(isset($lan[$ll][$per])) { unset($lan[$ll][$per]); continue; }
+		$allan[$ll].="<div>".'A'.$per."</div>";
 	}
-/*
+	foreach($lan as $ll=>$arper) foreach($arper as $cn=>$cv) $allan[$ll].="<div>".'D'.$cn." = ".h($cv)."</div>"; // предлагается удалить
 
-if(isset($con[$n])) unset($con[$n]); else $s.="<div>".'A'.'$'.$n."</div>"; }
-
-	$con=file_get_contents('config.php'); preg_match_all("/\n\s*".'\$'."([0-9a-z\_\-]+)\s*\=\s*([^\n]+)/si",$con,$m);
-	foreach($m[1] as $i=>$n) $con[$n]=$m[2][$i];
-	$s.="</td></tr></table><table><tr valign=top><td><b>config.php:</b></td><td>";
-	foreach($Uconf as $n=>$v) { if(isset($con[$n])) unset($con[$n]); else $s.="<div>".'A'.'$'.$n."</div>"; }
-	foreach($con as $n=>$l) { $s.="<div>".'D'.'$'.$n."=".h($l)."</div>"; } // предлагается удалить
-	unset($con);
-
-*/
-
-
-
-
-
-
-
-
-
-/*
+	foreach($allan as $ll=>$oo) $s.="</td></tr></table><table><tr valign=top><td><b>LANG:$ll:</b></td><td>".$oo;
+ 
+//=========================================================
 	// 3. Что с файлами?
 	foreach($Ufile as $f=>$d) {
 		$fhost=$GLOBALS['filehost'].$f; // физический файл
@@ -92,62 +73,7 @@ if(isset($con[$n])) unset($con[$n]); else $s.="<div>".'A'.'$'.$n."</div>"; }
 		} else $o='U';
 		$s.="<div>".$o.$fname."</div>";
 	}
-*/
-
-
-
-
-/*
-	
-	$f=explode(':',$file,3);
-		if(sizeof($f)==1) { // это файл
-			$ff=$GLOBALS['filehost'].$file;
-			$filename=basename($file);
-                        $dirname=dirname($file).'/';
-			$otstup=str_repeat(' ',substr_count($file,'/')*10);
-
-			if(!is_file($ff)) $ac='iia'; // add
-			else {
-				list($ftime,$fkey)=explode(' ',$val,2);
-//				if($ftime==filemtime($ff)) continue; // все с файлом нормалек
-				//else 
-				$ac='iiu'; // upgrade
-			}
-
-			if($dirname!=$lastdir) { $s.="<div class=\"$dirname ii1\" onclick='i_d(this)'>".$dirname."</div>"; $lastdir=$dirname; }
-
-			$s.="<div>".str_repeat(' ',strlen($dirname))
-			."<span id='e_$file' class=\"$dirname $ac\" onclick='i_f(this)'>".$filename."</span></div>";
-
-		} elseif($f[0]=='config') { // это конфиг
-		} elseif($f[0]=='lang') { // это язык
-		}
-
-	}
-
-*/
-
-// nextSibling
-// previousSibling
-// firstChild
-// lastChild
-/*
-
-className.split(' ');
-        if(typeof dc[1] != 'undefined' && dc[1].substring(0,2)=='ii'){ if(k==0) k=dc[1]=='ii1'?'ii0':'ii1'; i_d(p[i],k); }
-        }
-
-alert(p.length);
-
-for(var k=0,i=0;i<p.length;i++){ var dc=p[i].className.split(' ');
-        if(typeof dc[1] != 'undefined' && dc[1].substring(0,2)=='ii'){ if(k==0) k=dc[1]=='ii1'?'ii0':'ii1'; i_d(p[i],k); }
-        }
-
-
-		td1.style.backgroundColor='#ccc';
-
-
-*/
+//=========================================================
 
 $GLOBALS['selectjs']="
 
