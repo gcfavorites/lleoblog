04a23b086702565dbd635cc5cc9ad3891bdf005b
kns.tmb@gmail.com <kns.tmb@gmail.com@9663ea1c-021c-11df-8f7b-bfe16de6f99b>
2011-08-15 20:52:42 +0000

    update

--- include_sys/install_update.php
+++ include_sys/install_update.php
@@ -9,8 +9,8 @@ function UPDATE_testkey($key){ // безопасность: проверка ключа инсталляции
 	return $k!=$key?0:1;
 }
 
-function UPDATE_select($rrr) { $r=unserialize($rrr);
-	$s="<table><tr><td>files:";
+function UPDATE_select($rrr) { $r=unserialize($rrr); // return "<pre>".print_r($r,1);
+	$s="<table><tr><td><input type='button' onclick='i_submit()' value='INSTALL'>";
 	$otstup=''; $lastdir='';
 
 
@@ -21,13 +21,22 @@ function UPDATE_select($rrr) { $r=unserialize($rrr);
 	foreach($r as $n=>$l) { list($file,$val)=explode(' ',$l,2); unset($r[$n]);
 		if(strstr($file,':')) { // конфиг или язык
 			list($tt,$ff)=explode(':',$file,2);
-			if($tt='config') { $Uconf[$ff]=$val; continue; }
-			if($tt='lang') { $Ulang[$ff]=$val; continue; }
+			if($tt=='config') { $Uconf[$ff]=$val; continue; }
+			if($tt=='lang') { $Ulang[$ff]=$val; continue; }
 		}
 		$Ufile[$file]=$val;
 	}
 
-	// 1. Что с файлами?
+	// 1. Что с конфигом?
+	// config:msq_login $msq_login = ""; // "lleo";
+	$con=file_get_contents('config.php'); preg_match_all("/\n\s*".'\$'."([0-9a-z\_\-]+)\s*\=\s*([^\n]+)/si",$con,$m);
+	$con=array(); foreach($m[1] as $i=>$n) $con[$n]=$m[2][$i];
+	$s.="</td></tr></table><table><tr valign=top><td><b>config.php:</b></td><td>";
+	foreach($Uconf as $n=>$v) { if(isset($con[$n])) unset($con[$n]); else $s.="<div>".'A'.'$'.$n."</div>"; }
+	foreach($con as $n=>$l) { $s.="<div>".'D'.'$'.$n."=".h($l)."</div>"; } // предлагается удалить
+	unset($con);
+
+	// 2. Что с файлами?
 	foreach($Ufile as $f=>$d) {
 		$fhost=$GLOBALS['filehost'].$f; // физический файл
 		$fname=basename($f); // его имя
@@ -38,10 +47,12 @@ function UPDATE_select($rrr) { $r=unserialize($rrr);
 			$lastdir=$fdir;
 			// <div class=\"$dirname ii1\" onclick='i_d(this)'>".$dirname."</div>";
 		}
-		if(is_file($fhost)) $o='A';
+		if(is_file($fhost)) $o='D';
 		else $o='U';
 		$s.="<div>".$o.$fname."</div>";
 	}
+
+// lang:fido/ru:Comments:empty_comm Comments:empty_comm	А где же комментарий?
 
 
 /*
@@ -98,95 +109,51 @@ for(var k=0,i=0;i<p.length;i++){ var dc=p[i].className.split(' ');
 
 $GLOBALS['selectjs']="
 
-i_seldir=function(e){ alert(e.tagName); };
+i_submit=function(e){ var ee,td1,td2,dir,p,e,c,tr=idd('i_selectfiles').getElementsByTagName('TR'), s='';
+	for(var i=0;i<tr.length;i++){ p=tr[i]; td1=p.firstChild; td2=p.lastChild; if(td2==td1) continue;
+	dir=td1.firstChild.innerHTML;
+	ee=td2.getElementsByTagName('DIV'); for(var j=0;j<ee.length;j++){ e=ee[j];
+			s=s+'<br>'+dir+e.innerHTML.replace(/^<br>/g,'').replace(/\&nbsp;/g,' ').replace(/^ +(.+?) +$/g,'$1')+' | ';
+			if(e.style.textDecoration=='none') { s=s+e.style.color; }
+			else s=s+'------';
+		}
+	}
+	ohelpc('asd','sda',s);
+};
 
-i_sett=function(e,t){
-	e.style.cursor='pointer';
-	e.style.textDecoration='none';
-
-	e.setAttribute('tiptitle',t);
-	addEvent(e,'mouseover',function(){ idd('tip').innerHTML=this.getAttribute('tiptitle'); posdiv('tip',mouse_x+10,mouse_y+10); });
+i_sett=function(e,t){ e.style.cursor='pointer'; e.style.textDecoration='none'; e.setAttribute('tiptitle',t);
+	addEvent(e,'mouseover',function(){ idd('tip').innerHTML=this.getAttribute('tiptitle');
+	posdiv('tip',mouse_x+10,mouse_y+10); });
 	addEvent(e,'mouseout',function(){ zakryl('tip') } );
 	addEvent(e,'mousemove',function(){ posdiv('tip',mouse_x+10,mouse_y+10) } );
 }
 
-go_install=function(){ var t,tr=idd('i_selectfiles').getElementsByTagName('TR');
-
-	for(var i=0;i<tr.length;i++){ var p=tr[i];
-		var td1=p.firstChild; var td2=p.lastChild; if(td2==td1) continue;
-
-			var dir=td1.firstChild;
-				dir.onclick=function(){i_change_dir(this)};
-				i_sett(dir,'Select/Unselest all files');
-
-			var ee=td2.getElementsByTagName('DIV');
-			for(var j=0;j<ee.length;j++){ var pp=ee[j];
-				var l=pp.innerHTML; var O=l.substring(0,1); l=l.substring(1,l.length);
-
-				pp.setAttribute('myl',l);
-				pp.setAttribute('myO',O);
-
-				pp.innerHTML='<br>'+l;
-				pp.onclick=function(){i_change(this)};
-				pp.style.display='inline';
-
-				if(O=='U') { pp.style.color='green'; t='update'; }
-				else if(O=='A') { pp.style.color='red'; t='del'; }
-				else { pp.style.color='magenta'; t='unk'; }
-
-				i_sett(pp,t);
-
-				}
-	}
-	return;
-};
+go_install=function(){ var t,c,tr=idd('i_selectfiles').getElementsByTagName('TR');
+	for(var i=0;i<tr.length;i++){ var p=tr[i]; var td1=p.firstChild; var td2=p.lastChild; if(td2==td1) continue;
+		var dir=td1.firstChild; dir.onclick=function(){i_chand(this)}; i_sett(dir,'Select/Unselest all files');
+		var ee=td2.getElementsByTagName('DIV'); for(var j=0;j<ee.length;j++){ var x=ee[j];
+			var l=x.innerHTML; var O=l.substring(0,1); l=l.substring(1,l.length);
+			x.setAttribute('myl',l); x.setAttribute('myO',O);
+			x.innerHTML='<br>'+l; x.onclick=function(){i_chan(this)}; x.style.display='inline';
+				if(O=='U') { c='green'; t='update'; }
+				else if(O=='D') { c='red'; t='del'; }
+				else { c='magenta'; t='unk'; }
+			x.style.color=c; i_sett(x,t);
+}}};
 
 setTimeout(go_install,500);
 
-i_getopt=function(e){ var c=e.style.color; return e.getAttribute('tiptitle'); 
-alert(e.innerHTML+' '+i_getopt(e)+' '+e.style.textDecorationLineThrough);
-};
+i_chand=function(e){var p=e.parentNode.nextSibling.getElementsByTagName('DIV');for(var i=0;i<p.length;i++) i_change(p[i]) };
 
-
-i_change_dir=function(e){ var p=e.parentNode.nextSibling.getElementsByTagName('DIV');
-	for(var i=0;i<p.length;i++){i_change(p[i])}
-};
-
-i_change=function(e){ 
-	if(e.style.textDecoration=='none') {
-		e.style.textDecoration='line-through';
+i_chan=function(e){ 
+	if(e.style.textDecoration=='none'){ e.style.textDecoration='line-through';
 		e.innerHTML=e.innerHTML.replace(/^<br>(.+?)$/g,'<br>&nbsp;&nbsp;$1&nbsp;&nbsp;');
-	} else {
-		e.style.textDecoration='none';
+	}else{ e.style.textDecoration='none';
 		e.innerHTML=e.innerHTML.replace(/\&nbsp;/g,' ').replace(/^<br> +(.+?) +$/g,'<br>$1');
 	}
 };
-
-
-i_all=function(){
-var p=idd('packs').getElementsByTagName('div');
-for(var k=0,i=0;i<p.length;i++){ var dc=p[i].className.split(' ');
-	if(typeof dc[1] != 'undefined' && dc[1].substring(0,2)=='ii'){ if(k==0) k=dc[1]=='ii1'?'ii0':'ii1'; i_d(p[i],k); }
-	}
-};
-
-i_d=function(e,k){ var dc=e.className.split(' ');
-	if(typeof k == 'undefined') dc[1]=dc[1]=='ii1'?'ii0':'ii1'; else dc[1]=k;
-	e.className=dc.join(' ');
-	var p=getElementsByClass(dc[0]);
-	for(var i=1;i<p.length;i++) { var c=p[i].className.split(' ');
-		if(dc[1]=='ii0') c[2]='ii0';
-		else { if(c.length>2) { delete c[2]; c.length=2; } }
-		p[i].className=c.join(' ');
-	}
-};
-
-i_f=function(e){ var c=e.className.split(' ');
-	if(c.length>2) { delete c[2]; c.length=2; } else c[2]='ii0';
-	e.className=c.join(' ');
-};
 ";
-	return "<P onclick='this.style.textDecorationLineThrough=true;'>Click this if you</p><input type='button' onclick='go_install()' value='INSTALL'><div id='i_selectfiles'>$s</div>";
+	return "<div id='i_selectfiles'>$s</td></tr></table></div>";
 }
 
 ?>
